1751adc823d883c962a5dfd2c64b69bdfc5af7f4
Santiago Weight <santiago.weight@lmns.com>
2023-01-26 23:37:59 -0800

    kind of report dupes

--- core/Test/Tasty/Ingredients/ListTests.hs
+++ core/Test/Tasty/Ingredients/ListTests.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeApplications #-}
 module Test.Tasty.Ingredients.ListTests
   ( ListTests(..)
   , testsNames
@@ -24,6 +25,8 @@ import Data.Bifunctor (Bifunctor(..))
 import GHC.Generics
 import Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as BS
+import qualified Data.Map.Strict as Map
+import Data.Functor ((<&>))
 
 -- | This option, when set to 'True', specifies that we should run in the
 -- «list tests» mode.
@@ -82,23 +85,61 @@ srcLocToTestLoc :: SrcLoc -> TestLoc
 srcLocToTestLoc = \case
   SrcLoc {..} -> TestLoc {file = srcLocFile, line = srcLocStartLine, column = srcLocStartCol}
 
-data TestTreeInfoResult =
-  TestCaseInfo {name::TestName,loc:: TestLoc}
-  | TestGroupInfo {name::TestName,loc:: TestLoc,children:: [TestTreeInfoResult]}
+-- The names of the children must be unique
+newtype TestGroupChildren = UnsafeTestGroupChildren {unTestGroupChildren :: [TestTreeInfoResult]}
+  deriving newtype (ToJSON, Semigroup, Monoid)
+
+newtype DuplicateTestsError = DuplicateTestsError {dupes :: [String]}
   deriving stock (Generic)
   deriving anyclass ToJSON
+
+mkTestGroupChildren :: [TestTreeInfoResult] -> Either [String] TestGroupChildren
+mkTestGroupChildren childs =
+  let names = name <$> childs
+      nameFreqs = Map.fromListWith (+) $ zip names (repeat (1 :: Int))
+      dupedNames = fmap fst . filter ((> 1) . snd) $ Map.toList nameFreqs
+  in case dupedNames of
+      [] -> Right $ UnsafeTestGroupChildren childs
+      _ -> Left dupedNames
+
+
+data TestTreeInfoResult =
+  TestCaseInfo {name::TestName,loc:: TestLoc}
+  | TestGroupInfo {name::TestName,loc:: TestLoc,children:: TestGroupChildren}
+  deriving stock (Generic)
+  deriving anyclass ToJSON
+
+newtype TestTreeResult = TestTreeResult {unTestTreeResult :: Either [String] TestGroupChildren}
+  deriving newtype (ToJSON)
+
+instance Semigroup TestTreeResult where
+  (TestTreeResult t1) <> (TestTreeResult t2) = TestTreeResult $ case (t1, t2) of
+      (Left e1, Left e2) -> Left $ e1 <> e2
+      (Left e, _) -> Left e
+      (_, Left e) -> Left e
+      (Right r1, Right r2) -> Right $ r1 <> r2
+
+newtype TestTreeSuccess = TestTreeSuccess {tree :: TestGroupChildren}
+  deriving stock Generic
+  deriving anyclass (ToJSON)
+
+
+
+instance Monoid TestTreeResult where
+  mempty = TestTreeResult $ Right mempty
 
 -- | Obtain the list of all tests in the suite.
 --
 -- @since 0.4
-testTreeInfo :: OptionSet -> TestTree -> [TestTreeInfoResult]
-testTreeInfo {- opts -} {- tree -} =
-  foldTestTree
+testTreeInfo :: OptionSet -> TestTree -> TestTreeResult
+testTreeInfo {- opts -} {- tree -} opts tree = TestTreeResult $ do
+  testInfos <- unTestTreeResult $ foldTestTree
     trivialFold
-      { foldSingle = \ _opts (name, (_, srcLoc)) _test -> [TestCaseInfo name $ srcLocToTestLoc srcLoc]
+      { foldSingle = \ _opts (name, (_, srcLoc)) _test -> TestTreeResult $ Right $ UnsafeTestGroupChildren [TestCaseInfo name $ srcLocToTestLoc srcLoc]
       -- , foldGroup = \_opts groupName names -> map ((groupName ++ ".") ++) names
-      , foldGroup = \ _opts (groupName, (_, srcLoc)) names -> [TestGroupInfo groupName (srcLocToTestLoc srcLoc) names]
-      }
+      , foldGroup = \ _opts (groupName, (_, srcLoc)) (TestTreeResult childsEither) -> TestTreeResult $ childsEither >>= (\childs -> mkTestGroupChildren (unTestGroupChildren childs) <&> \checkedChilds -> UnsafeTestGroupChildren [TestGroupInfo groupName (srcLocToTestLoc srcLoc) checkedChilds])
+      } opts tree
+  mkTestGroupChildren $ unTestGroupChildren testInfos
 
 -- | The ingredient that provides the test listing functionality.
 --
@@ -109,7 +150,9 @@ reportTestTree = TestManager [Option (Proxy :: Proxy TestTreeInfo)] $
     case lookupOption opts of
       TestTreeInfo False -> Nothing
       TestTreeInfo True -> Just $ do
-        mapM_ (BS.putStrLn . encode) $ testTreeInfo opts tree
+        case testTreeInfo opts tree of
+          TestTreeResult (Left dupes) -> BS.putStrLn . encode $ DuplicateTestsError dupes
+          TestTreeResult (Right res) -> BS.putStrLn . encode $ TestTreeSuccess res
         return True
   where
     displayLocTestName (testName, (_, SrcLoc _ _ locFile startLine _ _ _)) = testName <> " " <> locFile <> " " <> show startLine
